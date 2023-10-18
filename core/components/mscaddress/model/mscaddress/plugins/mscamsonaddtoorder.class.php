e3e1a6f7d655b42cda79f832bd81f9b12d296957
webinmd <89poilo@gmail.com>
2023-10-18 14:44:54 +0300

    Check empty fields fix

--- core/components/mscaddress/model/mscaddress/plugins/mscamsonaddtoorder.class.php
+++ core/components/mscaddress/model/mscaddress/plugins/mscamsonaddtoorder.class.php
@@ -7,11 +7,11 @@ class mscaMsOnAddToOrder extends mscaPlugin
         $key = $this->modx->getOption('key', $this->scriptProperties);
 		$value = $this->modx->getOption('value', $this->scriptProperties);
 		
-		if ($key == 'msca_address' && !empty($value)){
+		if ($key == 'msca_address' && !empty($value)) {
 			if (!$this->modx->user->isAuthenticated()) {
 				return;
 			}
-			if (!$address = $this->modx->getObject('msCustomerAddress', ['id' => (int)$value, 'user_id' => $this->modx->user->get('id')])){
+			if (!$address = $this->modx->getObject('msCustomerAddress', ['id' => (int)$value, 'user_id' => $this->modx->user->get('id')])) {
 				return;
 			}
 			$order = $this->modx->getOption('order', $this->scriptProperties);
@@ -19,12 +19,12 @@ class mscaMsOnAddToOrder extends mscaPlugin
 			$fields = $this->modx->map['msCustomerAddress']['fields'];
 			unset($fields['id'],$fields['title'],$fields['user_id'],$fields['rank']);
 			
-			foreach (array_keys($fields) as $field){
-				if($addr_value = $address->get($field)) {
+			foreach (array_keys($fields) as $field) {
+				if ($addr_value = $address->get($field)) {
 					if ($field != 'properties') {
 						$order->add($field, $addr_value);
 					} else {
-						foreach ($addr_value as $prop_key => $prop_value){
+						foreach ($addr_value as $prop_key => $prop_value) {
 							$order->add($prop_key, $prop_value);
 						}
 					}
